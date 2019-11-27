541505f461878ca2d9e405adb0dbea1549814b77
sni-J <wjl0316@naver.com>
2019-11-27 04:18:59 +0000

    Updates

---
+++ precache-manifest.1f79be590df30daba29a3eb600ec84fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73cdd6e48f296bd42d599bcbc00e728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c754d7e4d08f637657c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b07598d545b7305ddb71",
+    "url": "/static/js/2.028095a0.chunk.js"
+  },
+  {
+    "revision": "1c754d7e4d08f637657c",
+    "url": "/static/js/main.12fc04ca.chunk.js"
+  },
+  {
+    "revision": "8b202ff5e3c5694adf56",
+    "url": "/static/js/runtime-main.555c328d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
