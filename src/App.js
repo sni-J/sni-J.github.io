8291831e2072dd5c40ab8bd428c5df42291c23e7
sni-J <wjl0316@naver.com>
2019-11-29 16:40:43 +0000

    feat(App): Refactor and apply Header, HomeContent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function App() {
+import Header from './components/organisms/Header'
+import HomeContent from './components/organisms/HomeContent'
+
+const App = () => {
+  const [mobile, setMobile] = useState(true)
+  const resize = () => {
+    setMobile(window.innerWidth < 768)
+  }
+
+  useEffect(() => {
+    resize()
+    window.addEventListener('resize', resize)
+  })
+
   return (
-    <div className="App">
-      <p>Hello World!</p>
+    <div>
+      <Header mobile={mobile} />
+      <HomeContent />
     </div>
   )
 }
