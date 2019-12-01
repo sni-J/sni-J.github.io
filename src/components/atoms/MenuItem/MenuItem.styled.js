28d68efd346634217627e154a98c53722f883d42
sni-J <wjl0316@naver.com>
2019-12-01 11:40:08 +0000

    feat(Header): Active item horz bar length change

--- src/components/atoms/MenuItem/MenuItem.styled.js
+++ src/components/atoms/MenuItem/MenuItem.styled.js
@@ -22,12 +22,12 @@ const MenuItemWrapper = styled.button`
       transition: all 0.3s ease-in-out;
 
       width: ${({ mobile, active }) =>
-        mobile ? '0.1rem' : active ? 'calc(100% + .8rem)' : '0%'};
+        mobile ? '0.1rem' : active ? 'calc(100% + .4rem)' : '0%'};
       height: ${({ mobile, active }) =>
         mobile ? (active ? 'calc(100% + .8rem)' : '0%') : '0.1rem'};
-      left: ${({ mobile, active }) => (mobile ? '0' : active ? '-.4rem' : '')};
+      left: ${({ mobile, active }) => (mobile ? '0' : active ? '-.2rem' : '')};
       top: ${({ mobile, active }) => (mobile ? (active ? '-.4rem' : '') : '')};
-      right: ${({ mobile }) => (mobile ? '0' : '-.4rem')};
+      right: ${({ mobile }) => (mobile ? '0' : '-.2rem')};
       bottom: ${({ mobile }) => (mobile ? '-.4rem' : '0')};
     }
   }
