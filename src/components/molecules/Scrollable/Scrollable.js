498c1bc2e0e3d0029aa523557714e9be12a44e83
sni-J <wjl0316@naver.com>
2019-12-08 05:18:12 +0900

    fix(Scrollable): Mouse Wheel strange move fix

--- src/components/molecules/Scrollable/Scrollable.js
+++ src/components/molecules/Scrollable/Scrollable.js
@@ -1,10 +1,43 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState, useEffect } from 'react'
 
 import ScrollableWrapper from './Scrollable.styled'
 
-const Scrollable = forwardRef(({ mobile, open, children }, ref) => {
+const Scrollable = forwardRef(({ open, children }, ref) => {
+  const [scrolling, setScrolling] = useState(0)
+
+  const scroll = () => {
+    const dest = Math.min(
+      1,
+      Math.max(ref.current.scrollTop + ref.current.clientHeight * scrolling, 0)
+    )
+
+    if (ref.current.scrollTop < dest) {
+      setScrolling(0)
+    }
+  }
+
+  const wheel = e => {
+    if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
+      setScrolling(Math.sign(e.deltaY))
+    }
+  }
+
+  useEffect(() => {
+    if (ref.current) {
+      if (typeof ref.current.scrollTo === 'function') {
+        ref.current.scrollTo({
+          top: ref.current.scrollTop + ref.current.clientHeight * scrolling,
+          behavior: 'smooth',
+        })
+      } else {
+        ref.current.scrollTop += ref.current.clientHeight * scrolling
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scrolling])
+
   return (
-    <ScrollableWrapper open={open} ref={ref}>
+    <ScrollableWrapper open={open} ref={ref} onScroll={scroll} onWheel={wheel}>
       {children}
     </ScrollableWrapper>
   )
