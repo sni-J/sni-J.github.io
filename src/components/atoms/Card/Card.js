ae558b0b37042edb9482d1db9acbdafd57121325
sni-J <wjl0316@naver.com>
2019-12-08 17:42:06 +0900

    refactor(static): Divide files for DevContent

--- src/components/atoms/Card/Card.js
+++ src/components/atoms/Card/Card.js
@@ -5,16 +5,16 @@ const Card = forwardRef(
   ({ mobile, name, meta, description, date, logo, bg, links, active }, ref) => {
     let logo_img
     try {
-      logo_img = require(`static/logo/${logo}`)
+      logo_img = require(`static/logo/DevContent/${logo}`)
     } catch (e) {
       logo_img = false
     }
 
     let background
     try {
-      background = require(`static/images/bg_img/${bg}`)
+      background = require(`static/images/DevContent/${bg}`)
     } catch (e) {
-      background = require(`static/images/bg_img/no_img.png`)
+      background = require(`static/images/DevContent/no_img.png`)
     }
 
     return (
